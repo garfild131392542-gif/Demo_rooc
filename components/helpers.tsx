3f4dc8ea30fad14be7eb17b060bc1c547f920199
Garfild <garfild.13139.2542@gmail.com>
2026-05-16 18:20:05 +0700

    ปรับชื่อรูปภาพ

--- components/helpers.tsx
+++ components/helpers.tsx
@@ -4,20 +4,20 @@ export function getJobIconUrl(jobName: string): string {
 
     // ตรวจสอบชื่ออาชีพและส่งคืนชื่อไฟล์รูปภาพที่ตรงกัน
     // รองรับการพิมพ์ผิดเล็กน้อย หรือชื่อเรียกที่ต่างกัน
-    if (job === 'paladin') return '/icons/jobs/paladin.png';
+    if (job === 'paladin') return '/icons/jobs/Paladin.png';
     if (['assassin'].includes(job)) return '/icons/jobs/Assassin.png';
     if (job === 'wizard') return '/icons/jobs/High Wizard.png';
-    if (['champion'].includes(job)) return '/icons/jobs/champion.png';
-    if (job === 'priest') return '/icons/jobs/priest.png';
-    if (job === 'bard') return '/icons/jobs/minstrel.png';
+    if (['champion'].includes(job)) return '/icons/jobs/Champion.png';
+    if (job === 'priest') return '/icons/jobs/Priest.png';
+    if (job === 'bard') return '/icons/jobs/Minstrel.png';
     if (job === 'gypsy') return '/icons/jobs/gypsy.png';
-    if (job === 'sniper') return '/icons/jobs/sniper.png';
-    if (['biochemist', 'creator'].includes(job)) return '/icons/jobs/biochemist.png';
-    if (['mastersmith', 'whitesmith'].includes(job)) return '/icons/jobs/mastersmith.png';
-    if (job === 'sage') return '/icons/jobs/professor.png';
+    if (job === 'sniper') return '/icons/jobs/Sniper.png';
+    if (['biochemist', 'creator'].includes(job)) return '/icons/jobs/Biochemist.png';
+    if (['mastersmith', 'whitesmith'].includes(job)) return '/icons/jobs/Mastersmith.png';
+    if (job === 'sage') return '/icons/jobs/Professor.png';
     if (['lord knight'].includes(job)) return '/icons/jobs/Lord Knight.png';
-    if (job === 'rogue') return '/icons/jobs/rogue.png';
-    if (job === 'summoner') return '/icons/jobs/summoner.png';
+    if (job === 'rogue') return '/icons/jobs/Stalker.png';
+    if (job === 'summoner') return '/icons/jobs/Summoner.png';
 
     // รูปภาพ Default กรณีสะกดชื่ออาชีพไม่ตรงหรือไม่พบรูป
     return '/icons/jobs/default.png';
